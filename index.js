f19bca25ae327ade7465f74bb5261ac0bd0a1d87
Frank <frankkashere@gmail.com>
2023-10-05 21:37:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = Math.floor(Math.random()*10);
+let num2 = Math.floor(Math.random()*10); 
+var multiply =(num1 * num2);
+multiply = 62;
+
+let random = Math.floor(Math.random()*10)+1;
+
+let num3 = Math.floor(Math.random()*10);
+let num4 = Math.floor(Math.random()*10);
+let mod = num3%num4;
+mod = 4;
+
+let max = Math.max(num1, num2, num3, num4);
+max = 20;
